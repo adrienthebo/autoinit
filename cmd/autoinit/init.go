c2a5139057b8d4bd8b250503b07634815a5ab65a
Adrien Thebo <adrien@lagrange-automation.io>
2022-12-15 19:10:09 +0000

    [autoinit/init] stub out init shell script

--- cmd/autoinit/init.go
+++ cmd/autoinit/init.go
@@ -3,15 +3,20 @@ package autoinit
 import (
 	"fmt"
 
+	_ "embed"
+
 	"github.com/spf13/cobra"
 )
+
+//go:embed init.bash
+var initBash string
 
 func InitCommand() *cobra.Command {
 	return &cobra.Command{
 		Use:   "init",
 		Short: "Initialize autoinit in your shell",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Hello!")
+			fmt.Println(initBash)
 		},
 	}
 }
